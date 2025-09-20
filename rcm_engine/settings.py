0fc5f72a7ebcc279d40dd0be31cc73a17d0bb4eb
Rotich Kibet <asbelkibetrotich@gmail.com>
2025-09-20 10:47:14 +0300

    added all to allowed hosts

--- rcm_engine/settings.py
+++ rcm_engine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*3ru9xzl8i3v9da9kxtj)kdmx)66&+b#6v=u+a!q5lumb1f4=0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
